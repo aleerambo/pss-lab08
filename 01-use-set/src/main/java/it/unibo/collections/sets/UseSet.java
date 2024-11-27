ca20bd5dabcf5a5a4192b14f1a5104d0b1625dc9
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2024-11-27 12:12:46 +0100

    UPDATE UseSet class

--- 01-use-set/src/main/java/it/unibo/collections/sets/UseSet.java
+++ 01-use-set/src/main/java/it/unibo/collections/sets/UseSet.java
@@ -1,6 +1,7 @@
 package it.unibo.collections.sets;
 
 import java.util.Collection;
+import java.util.TreeSet;
 
 /**
  * Example class using {@link java.util.Set}.
@@ -24,22 +25,54 @@ public final class UseSet {
          *
          * 1) Builds a TreeSet containing Strings
          */
+        final TreeSet<String> set = new TreeSet<String>();
+
         /*
          * 2) Populates such Collection with all the Strings representing numbers ranging from "1" to
          * "20" (both included)
          */
+        for(int i=1; i<=ELEMS; i++) {
+            set.add(String.valueOf(i));
+        }
+
         /*
          * 3) Prints its content
          */
+        System.out.println(set.toString());
+
         /*
          * 4) Removes all those strings whose represented number is divisible by three.
          * Note: the method removeIf(Predicate) is not allowed.
          */
+        for(int i=1; i<=ELEMS; i++) {
+            if(i%3 == 0) {
+                set.remove(String.valueOf(i));
+            }
+        }
+
         /*
          * 5) Prints the content of the Set using a for-each construct
          */
+        for (String element : set) {
+            System.out.println(element);
+        }
+
         /*
          * 6) Verifies whether all the numbers left in the set are even
          */
+        boolean allEven = true;
+        for(String element : set) {
+            int number = Integer.parseInt(element); // Converte la stringa in numero intero
+            if (number % 2 != 0) { // Controlla se il numero è dispari
+                allEven = false;
+                break; // Interrompi il ciclo appena trovi un dispari
+            }
+        }
+
+        if (allEven) {
+            System.out.println("All numbers in the set are even.");
+        } else {
+            System.out.println("Not all numbers in the set are even.");
+        }
     }
 }
